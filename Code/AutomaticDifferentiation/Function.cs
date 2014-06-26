e1a35521ece65526ca320337da81259b8aa5465c
renehernandez <renehr9102@gmail.com>
2014-06-25 21:10:39 -0700

    Adding Function and Variable class

---
+++ Code/AutomaticDifferentiation/Function.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimizer
+{
+    public class Function<T>
+    {
+
+        protected Func<Variable<T>, T> func;
+
+        public int Length { get; private set; }
+
+
+        public Function(Func<Variable<T>, T> func, int length)
+        {
+            this.func = func;
+            Length = length;
+        }
+
+        public T Evaluate(Variable<T> vector)
+        {
+            if(vector.Length != Length)
+                throw new InvalidOperationException();
+
+            return func(vector);
+        }
+
+    }
+}
