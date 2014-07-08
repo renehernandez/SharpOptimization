2e4aca80a036310a815801986f16c31aef118b45
renehernandez <renehr9102@gmail.com>
2014-07-07 22:13:47 -0700

    Adding heuristic, linesearch and correction namespace

--- Code/SharpOptimization.Optimizer/QnOptimizer.cs
+++ Code/SharpOptimization.Optimizer/QnOptimizer.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpOptimization.AutoDiff.Compiler;
+using SharpOptimization.Numeric;
 
 namespace SharpOptimization.Optimizer
 {
-    class QnOptimizer
+    public class QnOptimizer : AbstractOptimizer
     {
+        public QnOptimizer(int iterations) : base(iterations)
+        {
+        }
+
+        protected override Vector Minimize(CompiledFunc func, Vector input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
