f107b6c5b8df3a259ebf3f9a03d8b734ce631c34
renehernandez <renehr9102@gmail.com>
2014-07-07 17:34:09 -0700

    Updating autodiff to framework 4.0 and adding methods for optimizer

--- Code/SharpOptimization.Optimizer/AbstractOptimizer.cs
+++ Code/SharpOptimization.Optimizer/AbstractOptimizer.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpOptimization.AutoDiff.Compiler;
+using SharpOptimization.Numeric;
 
 namespace SharpOptimization.Optimizer
 {
     public abstract class AbstractOptimizer
     {
+
+        public int NumberOfIterations { get; private set; }
+
+        protected AbstractOptimizer(int iterations)
+        {
+            NumberOfIterations = iterations;
+        }
+
+        public Tuple<Vector, double> FindMinimun(CompiledFunc func, Vector input = null)
+        {
+            var res = Minimize(func, input);
+
+            return new Tuple<Vector, double>(res, func.Eval(res));
+        }
+
+        protected abstract Vector Minimize(CompiledFunc func, Vector input);
+
     }
 }
