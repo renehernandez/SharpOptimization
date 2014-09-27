c0aeee66e4fb356de66880b010363f1bc0506adb
renehernandez <renehr9102@gmail.com>
2014-09-27 14:18:43 -0400

    Adding base layout for MainWindow and adding RunCommand

---
+++ Code/SharpOptimization.Visual/Commands/RunCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace SharpOptimization.Visual.Commands
+{
+    public class RunCommand
+    {
+
+        private static RoutedUICommand requery;
+
+        public static RoutedUICommand Requery
+        {
+            get { return requery; }
+        }
+
+        static RunCommand()
+        {
+            var inputs = new InputGestureCollection
+            {
+                new KeyGesture(Key.F5, ModifierKeys.Control, "Ctrl + F5")
+            };
+
+            requery = new RoutedUICommand("Requery", "Requery",
+                typeof(RunCommand), inputs);
+        }
+    }
+}
---
+++ Code/SharpOptimization.Visual/ViewModels/PlotViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using OxyPlot;
+using SharpOptimization.Visual.Annotations;
+
+namespace SharpOptimization.Visual.ViewModels
+{
+    public class PlotViewModel : INotifyPropertyChanged
+    {
+        private PlotModel plotModel;
+
+        public PlotModel PlotModel
+        {
+            get { return plotModel; }
+            set { plotModel = value; OnPropertyChanged("PlotModel"); }
+        }
+
+        public PlotViewModel()
+        {
+            PlotModel = new PlotModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+
+    }
+}
